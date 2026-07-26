e96cc34a904f323e2f2fe39a2c64c5df4b357238
Oleksii Tonkonoh <bben24ua@gmail.com>
2026-07-26 15:38:21 +0300

    Added a few more tests

--- tests/test_predict.py
+++ tests/test_predict.py
@@ -9,11 +9,23 @@ def test_protected_endpoint_with_token(client, auth_headers):
     res = client.get("/predict/history", headers=auth_headers)
     assert res.status_code == 200
 
-def test_predict_with_token(post_prediction):
-    
-    assert post_prediction.status_code == 200
-    assert "prediction" in post_prediction.json()
-    assert "probability" in post_prediction.json()
+@pytest.mark.parametrize("payload", [
+    {
+        "note": "Example request",
+        "text": "NASA successfully launched a new rover to explore ancient riverbeds on Mars.",
+        "title": "NASA launches new Mars rover"
+    },
+    {
+        "note": "Another example",
+        "text": "Scientists discovered water on a distant planet.",
+        "title": "New discovery"
+    }
+])
+def test_predict_with_token(client, auth_headers, payload):
+    res = client.post("/predict/", json=payload, headers=auth_headers)
+    assert res.status_code == 200
+    assert "prediction" in res.json()
+    assert "probability" in res.json()
 
 def test_predict_without_token(client):
     post = {
@@ -25,4 +37,14 @@ def test_predict_without_token(client):
     res = client.post("/predict/", json=post)
 
     assert res.status_code == 401
+
+def test_predict_without_label(client, auth_headers):
+    post = {
+          "note": "Example request",
+          "text": "NASA successfully launched a new rover to explore ancient riverbeds on Mars."
+        }
+    res = client.post("/predict/", json=post, headers=auth_headers)
+
+    assert res.status_code == 422
+
 
